59cffd31db880ee44fdb12133b060e9b62409b43
Guillem Blasco <gowet92@gmail.com>
2025-11-20 15:21:19 +0100

    Add errorHandler to middlewares chain

--- src/server/app.ts
+++ src/server/app.ts
@@ -1,5 +1,11 @@
 import express from "express";
+import notFoundError from "./error/notFoundError/notFoundError.js";
+import errorHandler from "./error/errorHandler/errorHandler.js";
 
 const app = express();
+
+app.use(notFoundError);
+
+app.use(errorHandler);
 
 export default app;
