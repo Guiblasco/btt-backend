7f3e32e616184cbe05b176cb90b5b6380d847046
Guillem Blasco <gowet92@gmail.com>
2025-11-23 13:21:31 +0100

    Create healthChecker endpoint

--- src/server/app.ts
+++ src/server/app.ts
@@ -1,8 +1,14 @@
 import express from "express";
 import notFoundError from "./error/notFoundError/notFoundError.js";
 import errorHandler from "./error/errorHandler/errorHandler.js";
+import getPing from "./error/healthChecker/getping.js";
+import morgan from "morgan";
 
 const app = express();
+
+app.use(morgan("dev"));
+
+app.get("/", getPing);
 
 app.use(notFoundError);
 
