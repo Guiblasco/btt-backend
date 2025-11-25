3fcbd2735e1066cf0aeae6ac4a5da439bac595dd
Guillem Blasco <gowet92@gmail.com>
2025-11-25 18:13:18 +0100

    Create usersRouter

---
+++ src/Users/router/UsersRouter.ts
@@ -0,0 +1,13 @@
+import express from "express";
+import MongoUsersRepository from "../repository/MongoUsersRepository.js";
+import User from "../Model/User.js";
+import UsersController from "../Controller/UsersController.js";
+
+const userRouter = express.Router();
+
+const usersRepository = new MongoUsersRepository(User);
+const usersController = new UsersController(usersRepository);
+
+userRouter.get("/", usersController.getUsers);
+
+export default userRouter;
