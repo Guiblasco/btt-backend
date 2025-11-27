749c5dfdc2ae1fcd19c1fa8ed7656109b4a81be4
Guillem Blasco <gowet92@gmail.com>
2025-11-27 11:42:32 +0100

    Add new route at userRouter

--- src/Users/router/UsersRouter.ts
+++ src/Users/router/UsersRouter.ts
@@ -11,6 +11,7 @@ const usersController = new UsersController(usersRepository);
 userRouter.get("/", usersController.getUsers);
 userRouter.get("/:userId", usersController.getUserById);
 userRouter.post("/", usersController.createUser);
+userRouter.put("/:userId", usersController.updateUser);
 userRouter.delete("/:userId", usersController.deleteUser);
 
 export default userRouter;
