613662c29bf25e365025d179bf35fb5360880988
Guillem Blasco <gowet92@gmail.com>
2025-11-27 11:31:58 +0100

    Add updateUser method at controller

--- src/Users/Controller/types.ts
+++ src/Users/Controller/types.ts
@@ -1,9 +1,14 @@
 import type { Request } from "express";
-import type { UserCreateData } from "../types";
+import type { UserCreateData, UserUpdateData } from "../types";
 
 export type RequestWithUserId = Request<{ userId: string }>;
 export type RequestWithUserBody = Request<
   Record<string, unknown>,
   Record<string, unknown>,
   UserCreateData
+>;
+export type RequestWithUserUpdateBody = Request<
+  { userId: string },
+  Record<string, unknown>,
+  UserUpdateData
 >;
