b1d5e417be7d216fd7fb8309afc9543c393e1c09
Guillem Blasco <gowet92@gmail.com>
2025-11-27 11:16:24 +0100

    Add new method updateUser at repository

--- src/Users/repository/UsersRepository.ts
+++ src/Users/repository/UsersRepository.ts
@@ -1,8 +1,9 @@
-import type { UserCreateData, UserStructure } from "../types";
+import type { UserCreateData, UserStructure, UserUpdateData } from "../types";
 
 export interface UsersRepository {
   getAll(): Promise<UserStructure[]>;
   getUserById(id: string): Promise<UserStructure | null>;
   createUser(data: UserCreateData): Promise<UserStructure>;
+  updateUser(id: string, data: UserUpdateData): Promise<UserStructure | null>;
   deleteUser(id: string): Promise<boolean>;
 }
